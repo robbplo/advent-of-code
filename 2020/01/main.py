9ddd25ed155c5c182270e16ef9046fbbeb8353f1
Robbin Ploeger <robbplo@gmail.com>
2021-09-30 14:57:54 +0200

    part 2 day 1

--- 2020/01/main.py
+++ 2020/01/main.py
@@ -7,8 +7,18 @@ target = 2020
 
 print(input)
 
+## Part 1
+
+# for value in input:
+#     # print(value)
+#     for value2 in input:
+#         if (int(value) + int(value2) == target):
+#             print(int(value) * int(value2))
+
+## Part 2
+
 for value in input:
-    # print(value)
     for value2 in input:
-        if (int(value) + int(value2) == target):
-            print(int(value) * int(value2))
+        for value3 in input:
+            if (int(value) + int(value2) + int(value3) == target):
+                print(int(value) * int(value2) * int(value3))
